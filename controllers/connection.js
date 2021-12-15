045314b49721b891bd680cca8310b4ec275e39d0
SamarpanCoder2002 <samarpan2dasgupta@gmail.com>
2021-12-15 11:18:30 +0530

    For Connection Request Controller Made successfully

--- controllers/connection.js
+++ controllers/connection.js
@@ -1,8 +1,57 @@
-exports.ConnectionRequest = (req, res) => {
-    console.log(req.body.sender);
-    console.log(req.body.receiver);
+const { getAuth } = require("firebase/auth");
+const {
+    getFirestore,
+    collection,
+    getDocs,
+    getDoc,
+    doc,
+    setDoc,
+    where,
+    query,
+  } = require("firebase/firestore");
+const { User } = require("./types/auth-prototype");
 
-    return res.status(200).json({
-        message: "Connection request sent"
-    });
-}
+exports.ConnectionRequest = async (req, res) => {
+  const currentUser = req.body.currentUser;
+  const oppositeUser = req.body.oppositeUser;
+
+  const db = getFirestore();
+ 
+  await setDoc(
+    doc(
+      db,
+      User.usersCollection,
+      currentUser.id,
+      "connections",
+      "invitation",
+      "sent",
+      oppositeUser.id
+    ),
+    {
+        name: oppositeUser.name,
+        description: oppositeUser.description,
+        profilePic: oppositeUser.profilePic,
+    }
+  );
+  
+  await setDoc(
+    doc(
+      db,
+      User.usersCollection,
+      oppositeUser.id,
+      "connections",
+      "invitation",
+      "received",
+      currentUser.id,
+    ),
+    {
+        name: currentUser.name,
+        description: currentUser.description,
+        profilePic: currentUser.profilePic,
+    }
+  );
+
+  return res.status(200).json({     
+    message: "Connection Request Sent",
+  });
+};
