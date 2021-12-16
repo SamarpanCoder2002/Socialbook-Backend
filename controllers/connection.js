42581b6897e21dff8775cadd62924e96e0d1971c
SamarpanCoder2002 <samarpan2dasgupta@gmail.com>
2021-12-16 12:45:01 +0530

    get All Users with some error modified in connection request sent

--- controllers/connection.js
+++ controllers/connection.js
@@ -1,13 +1,10 @@
 const {
   getFirestore,
-  collection,
-  getDocs,
   getDoc,
   doc,
   setDoc,
-  where,
-  query,
-  deleteDoc,
+  collection,
+  getDocs,
 } = require("firebase/firestore");
 const { User, ConnectionType } = require("./types/types");
 
@@ -19,7 +16,8 @@ exports.connectionRequest = async (req, res) => {
     doc(db, User.usersCollection, req.auth.id, "connections", "list"),
     {
       [req.body.oppositeUser.id]: ConnectionType.sent,
-    }
+    },
+    { merge: true }
   );
 
   /// Adding under receive request section
@@ -33,7 +31,8 @@ exports.connectionRequest = async (req, res) => {
     ),
     {
       [req.auth.id]: ConnectionType.received,
-    }
+    },
+    { merge: true }
   );
 
   return res.status(200).json({
@@ -116,4 +115,57 @@ exports.getSpecificConnections = async (req, res) => {
   return res.status(200).json({
     data: incomingRequestData,
   });
+};
+
+exports.getAllAvailableUsers = async (req, res) => {
+  try {
+    const db = getFirestore();
+
+    const allUsersCollection = await getDocs(
+      collection(db, User.usersCollection)
+    );
+
+    const allDocId = [];
+
+    for (let i = 0; i < allUsersCollection.docs.length; i++) {
+      if (allUsersCollection.docs[i].id !== req.auth.id)
+        allDocId.push(allUsersCollection.docs[i].id);
+    }
+
+    const allRelatedDocCollection = await getDoc(
+      doc(db, User.usersCollection, req.auth.id, "connections", "list")
+    );
+
+    if (allRelatedDocCollection.exists()) {
+      const data = Object.entries(allRelatedDocCollection.data());
+      data.forEach(([docId, _]) => {
+        allDocId.splice(allDocId.indexOf(docId), 1);
+      });
+    }
+
+    const allAvailableUsersData = [];
+
+    for (let i = 0; i < allDocId.length; i++) {
+      const userRef = await getDoc(doc(db, User.usersCollection, allDocId[i]));
+
+      if (userRef.exists()) {
+        allAvailableUsersData.push({
+          id: allDocId[i],
+          name: userRef.data().name,
+          profilePic: userRef.data().profilePic,
+          description: userRef.data().description,
+        });
+      }
+    }
+
+    return res.status(200).json({
+      message: "All Available Users",
+      data: allAvailableUsersData,
+    });
+  } catch (err) {
+    console.log("Error in getAllAvailableUsers", err);
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
 };
--- routes/connection.js
+++ routes/connection.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { isSignedIn, isAuthenticated } = require("../controllers/auth");
-const { connectionRequest, acceptRequest,  getSpecificConnections } = require("../controllers/connection");
+const { connectionRequest, acceptRequest,  getSpecificConnections, getAllAvailableUsers } = require("../controllers/connection");
 
 router.post("/connect-request", isSignedIn, isAuthenticated, connectionRequest);
 router.post("/accept-request", isSignedIn, isAuthenticated, acceptRequest);
 router.get("/getConnections/:requiredConnectionType", isSignedIn, isAuthenticated, getSpecificConnections);
+router.get("/getAllAvailableUsers", isSignedIn, isAuthenticated, getAllAvailableUsers);
 
 module.exports = router;
