381851a9dc7776119432ff8d497397da46019bfa
SamarpanCoder2002 <samarpan2dasgupta@gmail.com>
2022-01-03 02:30:09 +0530

    Code Formatted

--- routes/post.js
+++ routes/post.js
@@ -18,26 +18,59 @@ const {
   getCurrentAccountPosts,
 } = require("../controllers/post-collection/get-posts");
 
-router.post("/createTextPost/:userId", isSignedIn, isAuthenticated, createTextPost);
-router.post("/createVideoPost/:userId", isSignedIn, isAuthenticated, createVideoPost);
+router.post(
+  "/createTextPost/:userId",
+  isSignedIn,
+  isAuthenticated,
+  createTextPost
+);
+router.post(
+  "/createVideoPost/:userId",
+  isSignedIn,
+  isAuthenticated,
+  createVideoPost
+);
 router.post(
   "/createDocumentPost/:userId",
   isSignedIn,
   isAuthenticated,
   createDocumentPost
 );
-router.post("/createPollPost/:userId", isSignedIn, isAuthenticated, createPollPost);
-
-// TODO: Come back here... not completed.. Do it when connect to frontend
-router.post("/createImagePost/:userId", isSignedIn, isAuthenticated, createImagePost);
-router.post("/createSlidePost/:userId", isSignedIn, isAuthenticated, createSlidePost);
+router.post(
+  "/createPollPost/:userId",
+  isSignedIn,
+  isAuthenticated,
+  createPollPost
+);
+router.post(
+  "/createImagePost/:userId",
+  isSignedIn,
+  isAuthenticated,
+  createImagePost
+);
+router.post(
+  "/createSlidePost/:userId",
+  isSignedIn,
+  isAuthenticated,
+  createSlidePost
+);
 
 // ** For Get Feed and Own Posts **
 router.get("/getFeedPosts/:userId", isSignedIn, isAuthenticated, getFeedPosts);
-router.get("/getMyPosts/:userId", isSignedIn, isAuthenticated, getCurrentAccountPosts);
+router.get(
+  "/getMyPosts/:userId",
+  isSignedIn,
+  isAuthenticated,
+  getCurrentAccountPosts
+);
 
 // ** For Engagement Inclusion **
-router.post("/postInsertLove/:postId/:userId", isSignedIn, isAuthenticated, insertLove);
+router.post(
+  "/postInsertLove/:postId/:userId",
+  isSignedIn,
+  isAuthenticated,
+  insertLove
+);
 router.post(
   "/postInsertComment/:postId/:userId",
   isSignedIn,
