05e76c91a38734b778e5d932a18d8528758a300c
SamarpanCoder2002 <samarpan2dasgupta@gmail.com>
2022-01-08 15:31:37 +0530

    Update Profile Data Controller Modified

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,38 +1,102 @@
 const { getFirestore, getDoc, doc, setDoc } = require("firebase/firestore");
 const { addNotification } = require("./notification");
-const { User } = require("./types/types");
+const { User, AccountThings } = require("./types/types");
+const formidable = require("formidable");
+const fs = require("fs");
+const { uploadFileInStorage } = require("./post-collection/upload-in-storage");
 
 exports.updateProfileData = (req, res) => {
-  const { name, description, profilePic, interests } = req.body;
-
   const db = getFirestore();
 
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+
   getDoc(doc(db, User.usersCollection, req.auth.id))
-    .then(async (docRef) => {
-      if (docRef.data()) {
-        await setDoc(
-          doc(db, User.usersCollection, req.auth.id),
-          {
-            name,
-            description,
-            profilePic,
-            interests,
-          },
-          { merge: true }
-        );
-
-        addNotification(
-          "Your Profile Updated Successfully 🎉",
-          `/${req.auth.id}/profile`,
-          req.auth.id
-        );
-
-        return res.status(200).json({
-          message: "Profile Updated",
+    .then((snapShot) => {
+      if (!snapShot.data()) {
+        return res.status(404).json({
+          code: 404,
+          message: "User Not Found",
         });
       }
-      return res.status(404).json({
-        message: "User Not Found",
+
+      const { name, description, profilePic } = snapShot.data();
+      form.parse(req, async (err, fields, file) => {
+        if (err) {
+          return res.status(400).json({
+            error: "Problem with Image",
+          });
+        }
+
+        const { updatedName, updatedDescription, updatedProfilePic } = fields;
+        const picFile = file.updatedProfilePic;
+
+        if (!picFile) {
+          if (
+            name !== updatedName ||
+            description !== updatedDescription ||
+            profilePic !== updatedProfilePic
+          ) {
+            await setDoc(
+              doc(db, User.usersCollection, req.auth.id),
+              {
+                name: updatedName,
+                description: updatedDescription,
+                profilePic: updatedProfilePic,
+              },
+              { merge: true }
+            );
+
+            return updatedProfileDataEndMessage(
+              updatedName,
+              updatedDescription,
+              updatedProfilePic,
+              req.auth.id,
+              res
+            );
+          }
+        } else {
+          if (picFile.size > 3000000) {
+            return res.status(400).json({
+              code: 400,
+              message:
+                "Profile Picture size too large... Please upload a Picture Within 3MB",
+            });
+          }
+
+          const uploadedProfilePicLink = await uploadFileInStorage(
+            fs.readFileSync(picFile.filepath),
+            `${req.auth.id}-profile-pic.jpg`,
+            AccountThings.profileImgStorageContainer,
+            picFile.mimetype
+          );
+
+          await setDoc(
+            doc(db, User.usersCollection, req.auth.id),
+            {
+              name: updatedName,
+              description: updatedDescription,
+              profilePic: uploadedProfilePicLink,
+            },
+            { merge: true }
+          );
+
+          return updatedProfileDataEndMessage(
+            updatedName,
+            updatedDescription,
+            uploadedProfilePicLink,
+            req.auth.id,
+            res
+          );
+        }
+
+        return updatedProfileDataEndMessage(
+          updatedName,
+          updatedDescription,
+          updatedProfilePic,
+          req.auth.id,
+          res
+        );
       });
     })
     .catch((err) => {
@@ -43,27 +107,52 @@ exports.updateProfileData = (req, res) => {
     });
 };
 
+const updatedProfileDataEndMessage = (
+  updatedName,
+  updatedDescription,
+  updatedProfilePic,
+  uid,
+  res
+) => {
+  addNotification(
+    "Your Profile Updated Successfully 🎉",
+    `/${uid}/profile`,
+    uid
+  );
+
+  return res.status(200).json({
+    code: 200,
+    message: "Profile Updated Successfully",
+    name: updatedName,
+    description: updatedDescription,
+    profilePic: updatedProfilePic,
+  });
+};
+
 exports.profileDataCollection = async (req, res) => {
-  const response = await this.getProfileData(req.auth.id, req.params.targetProfileId);
+  const response = await this.getProfileData(
+    req.auth.id,
+    req.params.targetProfileId
+  );
 
   return res.status(response.code).json(response);
 };
 
-exports.getProfileData = async ( authId, requiredProfileId) => {
+exports.getProfileData = async (authId, requiredProfileId) => {
   const db = getFirestore();
 
-  const docRef = await getDoc(doc(db, User.usersCollection, requiredProfileId)).catch(
-    (err) => {
-      console.log("error in getProfileData", err);
-      return {
-        code: 500,
-        message: "Internal Server Error",
-      };
-    }
-  );
+  const docRef = await getDoc(
+    doc(db, User.usersCollection, requiredProfileId)
+  ).catch((err) => {
+    console.log("error in getProfileData", err);
+    return {
+      code: 500,
+      message: "Internal Server Error",
+    };
+  });
 
   if (docRef.data()) {
-    const { name, description, profilePic, interests, email } = docRef.data();
+    const { name, description, profilePic, email } = docRef.data();
 
     const data = {
       name,
@@ -72,7 +161,6 @@ exports.getProfileData = async ( authId, requiredProfileId) => {
     };
 
     if (authId === requiredProfileId) {
-      data.interests = interests;
       data.email = email;
     }
 
